7c85343c584ef6b8485fa4b85ebe9f52250f988b
David Hollinger <david.hollinger123@gmail.com>
2022-04-13 10:30:14 -0700

    w2d3

--- Alg_practice.js
+++ Alg_practice.js
@@ -944,3 +944,146 @@ function reversingArr(arr) {
 console.log(reverseWordOrder(two_str1));
 console.log(reverseWordOrder(two_str2));
 console.log(reverseWordOrder(two_str3));
+
+//w2d3
+
+/* 
+  String: Rotate String
+  Create a standalone function that accepts a string and an integer,
+  and rotates the characters in the string to the right by that given
+  integer amount.
+*/
+
+const str = "Hello World";
+
+const rotateAmnt1 = 0;
+const expected1 = "Hello World";
+
+const rotateAmnt2 = 1;
+const expected2 = "dHello Worl";
+
+const rotateAmnt3 = 2;
+const expected3 = "ldHello Wor";
+
+const rotateAmnt4 = 4;
+const expected4 = "orldHello W";
+
+const rotateAmnt5 = 13;
+const expected5 = "ldHello Wor";
+/* 
+Explanation: this is 2 more than the length so it ends up being the same
+as rotating it 2 characters because after rotating every letter it gets back
+to the original position.
+*/
+
+/**
+ * Rotates a given string's characters to the right by the given amount,
+ * wrapping characters to the beginning.
+ * - Time: O(?).
+ * - Space: O(?).
+ * @param {string} str
+ * @param {number} amnt The amount of characters in the given str to rotate to the
+ *    right.
+ * @returns {string} The string rotated by the given amount.
+ */
+
+function rotateStr(string, amnt) {
+    if (amnt == 0){
+        return string;
+    }
+    if(amnt > string.length){
+        amnt -= string.length
+    }
+    let tempstr = "";
+    let newstr = ""
+    for(let i = 0; i <= amnt; i++){
+        tempstr += string.charAt(string.length-i-1);
+    }
+    newstr += tempstr
+    for(let j = 0; j < string.length; j++){
+        if(newstr.length < string.length){
+            newstr += string.charAt(j)
+        } else {
+            return newstr;
+        }
+    }
+}
+console.log(rotateStr("ABCD", 1))
+console.log(rotateStr("DABC", 1))
+console.log(rotateStr("CDAB", 1))
+console.log(rotateStr("BCDA", 1))
+console.log(rotateStr(str, rotateAmnt1))
+console.log(rotateStr(str, rotateAmnt2))
+console.log(rotateStr(str, rotateAmnt3))
+console.log(rotateStr(str, rotateAmnt4))
+console.log(rotateStr(str, rotateAmnt5))
+
+/* 
+  Create the function isRotation(str1,str2) that
+  returns whether the second string is a rotation of the first.
+*/
+
+const two_strA1 = "ABCD";
+const two_strB1 = "CDAB";
+// CDAB -> BCDA -> ABCD
+// Explanation: if you start from A in the 2nd string, the letters are in the same order, just rotated
+const two_expected1 = true;
+
+const two_strA2 = "ABCD";
+const two_strB2 = "CDBA";
+// CDBA -> ACDB -> BACD -> DBAC
+// Explanation: all same letters in 2nd string, but out of order
+const two_expected2 = false;
+
+const two_strA3 = "ABCD";
+const two_strB3 = "BCDAB";
+// Explanation: same letters in correct order but there is an extra letter.
+const two_expected3 = false;
+
+/**
+ * Determines whether the second string is a rotated version of the first.
+ * - Time: O(?).
+ * - Space: O(?).
+ * @param {string} s1
+ * @param {string} s2
+ * @returns {boolean} Whether the second string is a rotated version of the 1st.
+ */
+function isRotation(s1, s2) {
+    if(s1.length != s2.length){
+        return false
+    }
+    let temstr = "";
+    for(let i = 0; i <= s1.length; i++){
+        temstr = rotateStr(s2, 1)
+        s2 = temstr
+        if(s2 == s1){
+            return true
+        } 
+    }
+    return false;
+}
+
+function rotateStr(string, amnt) {
+    if (amnt == 0){
+        return string;
+    }
+    if(amnt > string.length){
+        amnt -= string.length
+    }
+    let tempstr = "";
+    let newstr = ""
+    for(let i = 0; i < amnt; i++){
+        tempstr += string.charAt(string.length-1);
+    }
+    newstr += tempstr
+    for(let j = 0; j < string.length; j++){
+        if(newstr.length < string.length){
+            newstr += string.charAt(j)
+        } else {
+            return newstr;
+        }
+    }
+}
+console.log(isRotation(two_strA1, two_strB1))
+console.log(isRotation(two_strA2, two_strB2))
+console.log(isRotation(two_strA3, two_strB3))
