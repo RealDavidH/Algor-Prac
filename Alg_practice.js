62752c2d0c910c3253ebd2df59f8d49b2e066eb7
David Hollinger <david.hollinger123@gmail.com>
2022-04-15 10:07:59 -0700

    w2d5

--- Alg_practice.js
+++ Alg_practice.js
@@ -1236,3 +1236,148 @@ function checkcloser(string){
 console.log(bracesValid(two_str1))
 console.log(bracesValid(two_str2))
 console.log(bracesValid(two_str3))
+
+
+
+
+/* 
+  Given a string that may have extra spaces at the start and the end,
+  return a new string that has the extra spaces at the start and the end trimmed (removed)
+  do not remove any other spaces.
+*/
+
+const str1 = "   hello world     ";
+const expected1 = "hello world";
+
+const str2 = "   Hi my name    is tyler     ";
+const expected2 = "Hi my name    is tyler";
+
+/**
+ * Trims any leading or trailing white space from the given str.
+ * - Time: O(?).
+ * - Space: O(?).
+ * @param {string} str
+ * @returns {string} The given string with any leading or trailing white space
+ *    stripped.
+ */
+ function trim(str){
+    let tempstr = ''
+    for(let i=1; i < str.length; i++){
+        if (str.charAt(1)  != ' ' || str.charAt(str.length) !=){
+            str.charAt(1) 
+        }
+        
+    }
+return tempstr
+}
+console.log(trim(str1))
+console.log(trim(str2))
+
+/* 
+  Given a string that may have extra spaces at the start and the end,
+  return a new string that has the extra spaces at the start and the end trimmed (removed)
+  do not remove any other spaces.
+*/
+
+const str1 = "   hello world     ";
+const expected1 = "hello world";
+
+const str2 = "   Hi my name    is tyler     ";
+const expected2 = "Hi my name    is tyler";
+
+/**
+ * Trims any leading or trailing white space from the given str.
+ * - Time: O(?).
+ * - Space: O(?).
+ * @param {string} str
+ * @returns {string} The given string with any leading or trailing white space
+ *    stripped.
+ */
+function trim(str) {
+    var result=str.split('');
+    var i=0
+    var j=str.length-1
+    while (str[i]==' '){
+            result.shift(str[i]);
+            i++;
+        }
+
+    while (str[j]==' '){
+        result.pop(str[i]);
+        j--;
+    }
+    
+    return result.join('');
+    }
+
+// console.log(trim(str1))
+// console.log(trim(str2))
+
+const two_strA1 = "yes";
+const two_strB1 = "eys";
+const two_expected1 = true;
+
+const two_strA2 = "yes";
+const two_strB2 = "eYs";
+const two_expected2 = true;
+
+const two_strA3 = "no";
+const two_strB3 = "noo";
+const two_expected3 = false;
+
+const two_strA4 = "silent";
+const two_strB4 = "listen";
+const two_expected4 = true;
+
+/**
+ * Determines whether s1 and s2 are anagrams of each other.
+ * Anagrams have all the same letters but in different orders.
+ * - Time: O(?).
+ * - Space: O(?).
+ * @param {string} s1
+ * @param {string} s2
+ * @returns {boolean} Whether s1 and s2 are anagrams.
+ */
+function isAnagram(s1, s2) {
+    if (s1.length !== s2.length){
+        return false
+    }
+    s1=s1.toLowerCase();
+    s2=s2.toLowerCase();
+    var output=''
+    for (i=0; i<s1.length; i++){
+        for (j=0; j<s2.length; j++){
+            if (s1[i]==s2[j]){
+                output+=s2[j]
+                console.log(output)
+            }
+        }
+    } if (s1==output){
+        return true;
+    } else return false
+}
+
+function isAnagramButBetter(s1,s2){ // tried to make a better verison
+    if (s1.length !== s2.length){
+        return false
+    }
+    var obj={
+        
+
+    }
+    for (var i=0; i<s1.length; i++){
+            obj[`value${i}`]=s1[i];
+    }
+    for (var j=0; j<s2.length; j++){
+        if (obj[`value${j}`]==s2[j]){
+            return true
+        }
+    }
+    return obj;
+}
+
+// console.log(isAnagram(two_strA1, two_strB1))
+// console.log(isAnagram(two_strA2, two_strB2))
+// console.log(isAnagram(two_strA3, two_strB3))
+// console.log(isAnagram(two_strA4, two_strB4))
+console.log(isAnagramButBetter('moon', 'mono'))
