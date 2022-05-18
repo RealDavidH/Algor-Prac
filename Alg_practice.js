e5302d8297043e0d2b3cd6090efd7f00f2579504
David Hollinger <david.hollinger123@gmail.com>
2022-05-18 10:05:04 -0700

    Alg

--- Alg_practice.js
+++ Alg_practice.js
@@ -2075,3 +2075,148 @@ console.log(findConsqSums(nums2, sum2));
 console.log(findConsqSums(nums3, sum3));
 console.log(findConsqSums(nums4, sum4));
 console.log(findConsqSums(nums5, sum5));
+
+/* 
+  Given an array of objects, a searchFor string, and searchBy key that exists
+  in the object return a new array of only those objects whose value for the
+  given key starts with the given search string.
+  You can assume the key will exist on the object and the value of that key
+  will be a string.
+  Bonus: make the search case insensitive.
+  Bonus: re-write it with functional programming, using built in methods.
+  Bonus: allow the search method to be provided as a parameter, e.g.,
+      string methods: includes, startsWith, endsWith.
+    - you can assume the searchMethod will be valid.
+  This kind of algorithm can be used in react onChange as you type into a
+  search bar to live-filter a list.
+*/
+
+
+// 1. start of w a new array
+// 2. Loop through the itemsArray
+// 3. We'll do something, probably
+// 4.
+const people = [
+    {
+      firstName: "John",
+      lastName: "Doe",
+    },
+    {
+      firstName: "Jane",
+      lastName: "Doe",
+    },
+    {
+      firstName: "Eddy",
+      lastName: "Lee",
+    },
+    {
+      firstName: "John",
+      lastName: "Fawn",
+    },
+    {
+      firstName: "Edward",
+      lastName: "Kim",
+    },
+  ];
+  
+  const searchFor1 = "Jo";
+  const searchBy1 = "firstName";
+  const expected1 = [
+    {
+      firstName: "John",
+      lastName: "Doe",
+    },
+    {
+      firstName: "John",
+      lastName: "Fawn",
+    },
+  ];
+  
+  const searchFor2 = "ohn";
+  const searchBy2 = "firstName";
+  const expected2 = [];
+  // Explanation: "John" contains "ohn", it does not start with "ohn"
+  
+  const searchFor3 = "Do";
+  const searchBy3 = "lastName";
+  const expected3 = [
+    {
+      firstName: "John",
+      lastName: "Doe",
+    },
+    {
+      firstName: "Jane",
+      lastName: "Doe",
+    },
+  ];
+  
+  // Bonus
+  const searchFor4 = "E";
+  const searchBy4 = "lastName";
+  const searchMethod4 = "includes";
+  const expected4 = [
+    {
+      firstName: "John",
+      lastName: "Doe",
+    },
+    {
+      firstName: "Jane",
+      lastName: "Doe",
+    },
+    {
+      firstName: "Eddy",
+      lastName: "Lee",
+    },
+  ];
+  
+  /**
+   * Filters the given items based on the search criteria using a startsWith
+   * search method.
+   * - Time: O(?).
+   * - Space: O(?).
+   * @param {Array<Object>} items The items to be filtered.
+   * @param {string} searchBy The key to search by.
+   * @param {string} searchFor The value of the given key to search for.
+   * @returns {Array<Objects>} The matched items.
+   */
+  function filterByKeyStartsWith(items, searchFor, searchBy) {
+    let newArr = []
+    for(const item of items){
+      let newStr = ''
+      for(let i=0; i<searchFor.length; i++) {
+        newStr += item[searchBy][i]
+      }
+      if(newStr === searchFor) {
+        newArr.push(item)
+      }
+    }
+    return newArr
+  }
+  // console.log(filterByKeyStartsWith(people, searchFor1, searchBy1))
+  // console.log(filterByKeyStartsWith(people, searchFor2, searchBy2))
+  // console.log(filterByKeyStartsWith(people, searchFor3, searchBy3))
+    // searchFor : value 
+    // searchBy: key to search
+    // Filters the given items based on the search criteria using a startsWith
+  
+  function filterByKey(items, searchFor, searchBy, searchMethod = "startsWith") {
+    let newArr = []
+    console.log(searchMethod)
+    for(const item of items){
+      if(item[searchBy][searchMethod](searchFor)) {
+        newArr.push(item)
+      }
+    }
+    if(newArr.length > 0){
+      return newArr
+    } else {
+      return false
+    }
+    
+  }
+  console.log(filterByKey(people, searchFor1, searchBy1))
+  console.log(filterByKey(people, searchFor2, searchBy2))
+  console.log(filterByKey(people, searchFor3, searchBy3))
+  console.log(filterByKey(people, searchFor4, searchBy4, searchMethod4))
+  
+  function functionalFilterByKey(items, searchFor, searchBy) {}
